92bc7ced45f859cc3b81d75fe9cf1779a592ef48
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2025-12-22 23:59:03 +0530

    ʜᴀʀʀʏ

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
-#harry
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "success",
+        "message": "EV Smart Charging Backend is running 🚀"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
